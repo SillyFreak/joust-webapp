1ceba7789ca9d91fecf8215466a68799dbe06031
SillyFreak <sillyfreak.ck@gmail.com>
2015-04-21 16:31:48 +0200

    add bracket pages

---
+++ src/main/java/at/pria/joust/webapp/web/joust/BracketController.java
@@ -0,0 +1,26 @@
+package at.pria.joust.webapp.web.joust;
+
+import at.pria.joust.Tournament;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BracketController {
+    @Autowired
+    private Tournament tournament;
+
+    @RequestMapping(value = "/admin/bracket/", method = RequestMethod.GET)
+    public String adminBracket(Model model) {
+        model.addAttribute("tournament", tournament);
+        return "joust/bracket_admin";
+    }
+
+    @RequestMapping(value = "/bracket/", method = RequestMethod.GET)
+    public String bracket(Model model) {
+        model.addAttribute("tournament", tournament);
+        return "joust/bracket";
+    }
+}
