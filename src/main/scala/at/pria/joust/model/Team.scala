fc4ea45f6706e7555adc5dcc155b8b1e5b3db11c
SillyFreak <sillyfreak.ck@gmail.com>
2015-04-25 00:38:49 +0200

    reimplement seeding average (and max)

--- src/main/scala/at/pria/joust/model/Team.scala
+++ src/main/scala/at/pria/joust/model/Team.scala
@@ -7,15 +7,17 @@
 package at.pria.joust.model
 
 import scala.beans.BeanProperty
+import scala.collection.JavaConversions._
 
 import org.hibernate.validator.constraints.NotEmpty
 import org.springframework.data.repository.CrudRepository
 
 import javax.persistence.Entity
-import javax.persistence.Id
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
+import javax.persistence.Id
 import javax.persistence.OneToMany
+import javax.persistence.Transient
 
 import java.lang.{ Long => jLong }
 import java.util.{ List => juList, ArrayList => juArrayList }
@@ -47,6 +49,26 @@ class Team {
 
   @OneToMany(mappedBy = "team")
   @BeanProperty var seedingGames: juList[SeedingGame] = new juArrayList[SeedingGame]
+
+  private[this] def seedingStats = {
+    val allScores =
+      for (game <- List(seedingGames: _*))
+        yield if (game.finished) Some(game.score) else None
+    val scores = allScores.collect { case Some(x) => x }.sorted
+
+    if (scores.isEmpty) {
+      (0, 0d)
+    } else {
+      val dropped = scores.takeRight(2)
+      (dropped.last, dropped.sum.toDouble / dropped.size)
+    }
+  }
+
+  def seedingMax = seedingStats._1
+  @Transient def getSeedingMax() = seedingMax
+
+  def seedingAvg = seedingStats._2
+  @Transient def getSeedingAvg() = seedingAvg
 }
 
 trait TeamRepository extends CrudRepository[Team, jLong] {
