70abf3d35c34c5c1ce817256d8f6af220ba8f954
SillyFreak <sillyfreak.ck@gmail.com>
2015-04-25 11:24:24 +0200

    change (non-graphical) bracket to use nodel classes

--- src/main/scala/at/pria/joust/web/BracketController.scala
+++ src/main/scala/at/pria/joust/web/BracketController.scala
@@ -1,8 +1,10 @@
 package at.pria.joust.web
 
 import scala.beans.BeanProperty
+import scala.collection.JavaConversions._
 
-import at.pria.joust._
+import at.pria.joust.model._
+import at.pria.joust.service.BracketStructure
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -10,118 +12,136 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-import scala.collection.JavaConversions._
 import java.util.{ List => juList }
 
 @Controller
 class BracketController {
   @Autowired
-  private[this] var tournament: Tournament = _
+  private[this] var tournamentRepo: TournamentRepository = _
+  @Autowired
+  private[this] var gameRepo: GameRepository = _
 
   @RequestMapping(value = Array("/admin/bracket/"), method = Array(RequestMethod.GET))
   def bracketAdmin(model: Model) = {
-    model.addAttribute("bracket", new Bracket())
-    model.addAttribute("ByeTeam", ByeTeam)
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+    val games = bracket.games.map { BracketGameView(_) }
+
+    model.addAttribute("bracket", games: juList[BracketGameView])
     model.addAttribute(new BracketControllerInput())
     "joust/bracket_admin"
   }
 
   @RequestMapping(value = Array("/admin/bracket/"), method = Array(RequestMethod.POST))
-  def bracketAdminPost(model: Model, bracketControllerInput: BracketControllerInput) = {
-    bracketControllerInput.apply(tournament)
+  def bracketAdminPost(model: Model, in: BracketControllerInput) = {
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+
+    val game = bracket.games(in.id).game
+    game.finished = in.resolved
+    game.winnerSideA = in.winnerSideA
+    gameRepo.save(game)
+
     bracketAdmin(model)
   }
 
   @RequestMapping(value = Array("/bracket/"), method = Array(RequestMethod.GET))
   def bracket(model: Model) = {
-    model.addAttribute("bracket", new Bracket())
-    model.addAttribute("ByeTeam", ByeTeam)
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+    val games = bracket.games.map { BracketGameView(_) }
+
+    model.addAttribute("bracket", games: juList[BracketGameView])
     "joust/bracket"
   }
 
   @RequestMapping(value = Array("/bracket/main/"), method = Array(RequestMethod.GET))
   def mainBracket(model: Model) = {
-    model.addAttribute("bracket", new Bracket())
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+
     model.addAttribute("mode", "main")
-    model.addAttribute("ByeTeam", ByeTeam)
     "joust/bracket2"
   }
 
   @RequestMapping(value = Array("/bracket/consolation/"), method = Array(RequestMethod.GET))
   def consolationBracket(model: Model) = {
-    model.addAttribute("bracket", new Bracket())
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+
     model.addAttribute("mode", "consolation")
-    model.addAttribute("ByeTeam", ByeTeam)
     "joust/bracket2"
   }
 
   @RequestMapping(value = Array("/bracket/final/"), method = Array(RequestMethod.GET))
   def finalBracket(model: Model) = {
-    model.addAttribute("bracket", new Bracket())
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+
     model.addAttribute("mode", "final")
-    model.addAttribute("ByeTeam", ByeTeam)
     "joust/bracket2"
   }
 
-  case class BracketMatchView(
-      @BeanProperty id: Int,
-      @BeanProperty ord: Int,
-      @BeanProperty aTeam: TeamLike,
-      @BeanProperty bTeam: TeamLike,
-      @BeanProperty winner: TeamLike) {
+  case class BracketGameView(
+      @BeanProperty game: BracketStructure#BracketGame) {
+    def getId() = game.id
 
-    def this(game: BracketMatch) =
-      this(game.id, game.ord, game.getATeam(), game.getBTeam(), tournament.bracketResults.result(game) match {
-        case None                               => null
-        case Some(BracketMatchResult(_, true))  => game.getATeam()
-        case Some(BracketMatchResult(_, false)) => game.getBTeam()
-      })
-  }
-
-  private class Bracket {
-    private[this] val m = tournament.bracketMatches.map(new BracketMatchView(_))
-
-    private[this] val brackets = {
-      val _rounds = m.groupBy(_.ord)
-      val size = _rounds.size
-      val rounds =
-        _rounds.groupBy {
-          case (ord, a) => ord match {
-            case 0                  => "main"
-            case x if x >= size - 3 => "final"
-            case x if x % 3 == 1    => "main"
-            case _                  => "consolation"
-          }
-        }
-
-      rounds.map {
-        case (k, round) =>
-          val bracket =
-            round.toList
-              .sortBy { case (k, _) => k }
-              .map {
-                case (_, v) => v: juList[BracketMatchView]
-              }
-          (k, (if (k == "consolation") bracket.reverse else bracket): juList[juList[BracketMatchView]])
+    def isScorable() = {
+      val a = getATeam()
+      val b = getBTeam()
+      (a, b) match {
+        case (Some(a), Some(b)) if a != null && b != null => true
+        case _ => false
       }
     }
 
-    @BeanProperty val games = m: juList[BracketMatchView]
+    def isFinished() = game.finished
 
-    @BeanProperty val mainRounds = brackets("main")
-    @BeanProperty val consolationRounds = brackets("consolation")
-    @BeanProperty val finalRounds = brackets("final")
+    def getATeam() = game.aTeam()
+    def getBTeam() = game.bTeam()
+
+    def getWinner() = game.winner()
+    def getLoser() = game.loser()
   }
+
+  //private class Bracket {
+  //  private[this] val m = tournament.bracketMatches.map(new BracketMatchView(_))
+  //
+  //  private[this] val brackets = {
+  //    val _rounds = m.groupBy(_.ord)
+  //    val size = _rounds.size
+  //    val rounds =
+  //      _rounds.groupBy {
+  //        case (ord, a) => ord match {
+  //          case 0                  => "main"
+  //          case x if x >= size - 3 => "final"
+  //          case x if x % 3 == 1    => "main"
+  //          case _                  => "consolation"
+  //        }
+  //      }
+  //
+  //    rounds.map {
+  //      case (k, round) =>
+  //        val bracket =
+  //          round.toList
+  //            .sortBy { case (k, _) => k }
+  //            .map {
+  //              case (_, v) => v: juList[BracketMatchView]
+  //            }
+  //        (k, (if (k == "consolation") bracket.reverse else bracket): juList[juList[BracketMatchView]])
+  //    }
+  //  }
+  //
+  //  @BeanProperty val games = m: juList[BracketMatchView]
+  //
+  //  @BeanProperty val mainRounds = brackets("main")
+  //  @BeanProperty val consolationRounds = brackets("consolation")
+  //  @BeanProperty val finalRounds = brackets("final")
+  //}
 }
 
 class BracketControllerInput {
   @BeanProperty var id: Int = _
   @BeanProperty var winnerSideA: Boolean = _
   @BeanProperty var resolved: Boolean = _
-
-  def apply(t: Tournament) = {
-    val game = t.getBracketMatches().get(id)
-    val result = if (resolved) Some(winnerSideA) else None
-    t.bracketResults.result(game, result)
-  }
 }
