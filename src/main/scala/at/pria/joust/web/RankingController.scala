ca5c356f96d5ad3a342143e8453cd99c0751f3ff
SillyFreak <sillyfreak.ck@gmail.com>
2015-04-25 13:36:41 +0200

    update ranking

--- src/main/scala/at/pria/joust/web/RankingController.scala
+++ src/main/scala/at/pria/joust/web/RankingController.scala
@@ -2,7 +2,8 @@ package at.pria.joust.web
 
 import scala.beans.BeanProperty
 
-import at.pria.joust._
+import at.pria.joust.model._
+import at.pria.joust.service.BracketStructure
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
@@ -16,27 +17,47 @@ import java.util.{ List => juList }
 @Controller
 class RankingController {
   @Autowired
-  private[this] var tournament: Tournament = _
+  private[this] var tournamentRepo: TournamentRepository = _
 
   @RequestMapping(value = Array("/ranking/"), method = Array(RequestMethod.GET))
   def ranking(model: Model) = {
-    val ov = tournament.overallResults.ranking
-    val doc = tournament.documentationResults.ranking
-    val seed = tournament.seedingResults.ranking
-    val br = tournament.bracketResults.ranking
+    val tournament = tournamentRepo.findByName("Botball")
+    val bracket = new BracketStructure(tournament)
+
+    val teams = tournament.teams.toList.map { TeamRanks(bracket, _) }
+
+    val byOverall = teams.sortBy(_.overallRank)
+    val byDoc = teams.sortBy(_.docRank)
+    val bySeeding = teams.sortBy(_.seedingRank)
+    val byBracket = teams.sortBy(_.bracketRank)
 
     val ranking =
-      for ((((o, d), s), b) <- (ov zip doc zip seed zip br))
+      for ((((o, d), s), b) <- (byOverall zip byDoc zip bySeeding zip byBracket))
         yield RankItem(o, d, s, b)
 
-    model.addAttribute("tournament", tournament)
     model.addAttribute("ranking", ranking: juList[RankItem])
     "joust/ranking"
   }
 
   case class RankItem(
-    @BeanProperty overall: OverallScore,
-    @BeanProperty doc: DocumentationScore,
-    @BeanProperty seed: SeedingScore,
-    @BeanProperty de: BracketScore)
+    @BeanProperty overall: TeamRanks,
+    @BeanProperty doc: TeamRanks,
+    @BeanProperty seeding: TeamRanks,
+    @BeanProperty bracket: TeamRanks)
+
+  case class TeamRanks(
+      bracket: BracketStructure,
+      @BeanProperty team: Team) {
+    @BeanProperty val overallScore = team.overallScore(bracket)
+    @BeanProperty val overallRank = team.overallRank(bracket)
+
+    @BeanProperty val docScore = team.docScore
+    @BeanProperty val docRank = team.docRank
+
+    @BeanProperty val seedingScore = team.seedingScore
+    @BeanProperty val seedingRank = team.seedingRank
+
+    @BeanProperty val bracketScore = team.bracketScore(bracket)
+    @BeanProperty val bracketRank = team.bracketRank(bracket)
+  }
 }
